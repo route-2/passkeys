a8def0bb316aaef5b4e577257ad349e6f710c990
Arch0125 <dasarchisman25@gmail.com>
2023-12-09 12:16:47 +0530

    update parent url

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,22 @@ export default function Home() {
   const generatePasskey = async () => {
     // let popup = window.open("http://localhost:3001","Generating passkey","height=400,width=600,resizable=yes")
     console.log("Generating passkey...");
-    const res = await ClientRegistration("abcd");
+    let params = new URLSearchParams(window.location.search);
+    let parentUrl = params.get('parentUrl');
+    let username = params.get('username');
+    const res = await ClientRegistration(username||"test");
     console.log(res);
+    
+    window.opener.postMessage(res, parentUrl);
   };
 
   generatePasskey();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <button
-        className="px-4 py-2 bg-white text-black rounded-xl"
-        onClick={generatePasskey}
-      >
-        Generate Passkey
-      </button>
+      <p>
+        Generating passkey ...
+      </p>
     </main>
   );
 }
